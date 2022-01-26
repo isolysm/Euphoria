ec43c739f843a05a4ca4e286dd1e5df14d9841bc
yujin <84204420+euphorxa@users.noreply.github.com>
2022-01-26 12:43:11 -0800

    Discord is down so back to this and being a clown

---
+++ root.gradle.kts
@@ -0,0 +1,14 @@
+// Relies on preprocess to map to different versions of Minecraft, also did a funny with serialization
+
+plugins {
+    kotlin("jvm") version VERSION.KOTLIN apply false
+    kotlin("multiplatform") version VERSION.KOTLIN apply false
+    kotlin("plugin.serialization") version VERSION.KOTLIN apply false
+    id("com.replaymod.preprocess") version "7746c47"
+}
+
+configurations.register("compileClasspath")
+
+preprocess {
+    "1.18.1"(11801, "srg")
+}
