61fd1a343785b6c1a485ea439a9cbe2c9a14c025
yujin <84204420+euphorxa@users.noreply.github.com>
2022-01-26 14:17:41 -0800

    You know I had to skid preprocessor just for it to work and even then it might not even work

--- root.gradle.kts
+++ root.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     kotlin("multiplatform") version VERSION.KOTLIN apply false
     kotlin("plugin.serialization") version VERSION.KOTLIN apply false
     id("com.replaymod.preprocess") version "7746c47"
+    id("org.spongepowered.mixin) version "d5f9873d60"
 }
 
 configurations.register("compileClasspath")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,21 @@
-rootProject.name = "Euphoria"
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+        maven("https://jitpack.io")
+        maven("https://maven.fabricmc.net")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "com.replaymod.preprocess" -> {
+                    useModule("com.github.replaymod:preprocessor:${requested.version}")
+                }
+            }
+        }
+    }
+}
+
+rootProject.buildFileName = "root.gradle.kts"
