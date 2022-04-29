3e96f88a0cd145061330431c0ea5b18c6aa80dad
vispin <92119905+vispin@users.noreply.github.com>
2022-04-29 08:01:07 -0700

    Well that's fair

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.6.21" apply false
-    kotlin("plugin.serialization") version "1.6.21" apply false
+    kotlin("jvm") version "1.6.10" apply false
+    kotlin("plugin.serialization") version "1.6.10" apply false
 
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
     id("gg.essential.multi-version.root")
