9e9bc6378ad3329938a67d63180b2ec1a88e3339
yujin <84204420+shuuyu@users.noreply.github.com>
2022-06-30 13:37:29 -0700

    Update dependencies myself

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.6.21" apply false
-    kotlin("plugin.serialization") version "1.6.21" apply false
+    kotlin("jvm") version "1.7.0" apply false
+    kotlin("plugin.serialization") version "1.7.0" apply false
 
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("gg.essential.multi-version.root") version "0.1.9"
+    id("gg.essential.multi-version.root") version "0.1.10"
     id("com.modrinth.minotaur") version "2.4.0" apply false
     // id("com.replaymod.preprocess") version "73d8bed"
     // id("fabric-loom") version "0.12-SNAPSHOT" apply false
